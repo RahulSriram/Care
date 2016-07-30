3288d853ade42976d26aa1221860df606180f868
sreevathsadb <srivathsa2396@gmail.com>
2016-07-30 15:40:24 +0530

    minor changes

--- app/src/main/java/tech/rahulsriram/care/LoginActivity.java
+++ app/src/main/java/tech/rahulsriram/care/LoginActivity.java
@@ -1,24 +1,28 @@
 package tech.rahulsriram.care;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-    String get_num;
-    TextView text;
-    Button b;
-
-}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        TextView text = (TextView)findViewById(R.id.mn);
-        
+        TextView text = (TextView) findViewById(R.id.mobileNumberInput);
 
-
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent callMapsActivity = new Intent(LoginActivity.this, MapsActivity.class);
+                LoginActivity.this.startActivity(callMapsActivity);
+            }
+        });
     }
+}
 
