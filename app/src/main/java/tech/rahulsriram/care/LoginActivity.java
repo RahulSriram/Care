c9365fe13990907e77c5e787d7404e3751407baa
sreevathsadb <srivathsa2396@gmail.com>
2016-08-15 23:01:18 +0530

    working

--- app/src/main/java/tech/rahulsriram/care/LoginActivity.java
+++ app/src/main/java/tech/rahulsriram/care/LoginActivity.java
@@ -2,31 +2,50 @@ package tech.rahulsriram.care;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    String phoneNumber,deviceId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        final EditText mobileNumber = (EditText) findViewById(R.id.mobileNumberInput);
-
-        Button loginButton = (Button) findViewById(R.id.loginButton);
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        Button login =(Button)findViewById(R.id.loginButton);
+        assert login != null;
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Number: " + mobileNumber.getText().toString(), Toast.LENGTH_LONG).show();
-                Intent callMapsActivity = new Intent(LoginActivity.this, MapsActivity.class);
-                startActivity(callMapsActivity);
-                finish();
+
+                Toast.makeText(LoginActivity.this,getPhoneNumber(), Toast.LENGTH_LONG).show();
+                Toast.makeText(LoginActivity.this,getDeviceId(), Toast.LENGTH_LONG).show();
+                Intent intent =new Intent(LoginActivity.this,ItemSelectionActivity.class);
+                startActivity(intent);
+
             }
         });
+
     }
+    public String getPhoneNumber()
+    {
+        EditText pn = (EditText)findViewById(R.id.mobileNumberInput);
+        phoneNumber = pn.getText().toString();
+        return phoneNumber;
+
+    }
+
+    public String getDeviceId()
+    {
+
+        deviceId = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
+
+          return deviceId;
+    }
+
 }
 
