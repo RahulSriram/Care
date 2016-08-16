89e5bc1d3a7e980ed6b5ca36c4b2d10693123c6e
sreevathsadb <srivathsa2396@gmail.com>
2016-08-16 19:47:33 +0530

    itemSection module is intended

---
+++ app/src/main/java/tech/rahulsriram/care/newFeedActivity.java
@@ -0,0 +1,13 @@
+package tech.rahulsriram.care;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class newFeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_feed);
+    }
+}
