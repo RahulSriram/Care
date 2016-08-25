4712daf4f4b69df7d30e8f98f048c1d9665b2d5f
jebin2 <jebineinstein@gmail.com>
2016-08-25 19:41:20 +0530

    floating bar corrected in closed donation

--- app/src/main/java/tech/rahulsriram/care/SettingsActivity.java
+++ app/src/main/java/tech/rahulsriram/care/SettingsActivity.java
@@ -1,8 +1,8 @@
 package tech.rahulsriram.care;
 
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
